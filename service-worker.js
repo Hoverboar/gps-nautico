b7cacbcc65977cffb2c824aa95abc4cf54a56377
Hoverboar <101845892+Hoverboar@users.noreply.github.com>
2026-08-26 21:43:22 -0300

    Update service worker to cache additional files

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
 const CACHE = "gps-nautico-v2";
 
-
 const ARQUIVOS = [
   "./",
   "./index.html",
-  "./service-worker.js"
+  "./service-worker.js",
+  "https://unpkg.com/leaflet/dist/leaflet.js",
+  "https://unpkg.com/leaflet/dist/leaflet.css"
 ];
 
 
 self.addEventListener("install", function(event){
 
-  event.waitUntil(
+event.waitUntil(
 
-    caches.open(CACHE).then(function(cache){
+caches.open(CACHE).then(function(cache){
 
-      return cache.addAll(ARQUIVOS);
+return cache.addAll(ARQUIVOS);
 
-    })
+})
 
-  );
+);
 
 });
 
@@ -26,15 +27,16 @@ self.addEventListener("install", function(event){
 
 self.addEventListener("fetch", function(event){
 
-  event.respondWith(
+event.respondWith(
 
-    caches.match(event.request)
-    .then(function(resposta){
+caches.match(event.request)
 
-      return resposta || fetch(event.request);
+.then(function(resposta){
 
-    })
+return resposta || fetch(event.request);
 
-  );
+})
+
+);
 
 });
