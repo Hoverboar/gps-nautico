b36eac1b022627e60b3489661e54d6681e922f34
Hoverboar <101845892+Hoverboar@users.noreply.github.com>
2026-08-26 21:24:41 -0300

    Implement service worker with caching functionality

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,40 @@
 
+const CACHE = "gps-nautico-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./service-worker.js"
+];
+
+
+self.addEventListener("install", function(event){
+
+  event.waitUntil(
+
+    caches.open(CACHE).then(function(cache){
+
+      return cache.addAll(ARQUIVOS);
+
+    })
+
+  );
+
+});
+
+
+
+self.addEventListener("fetch", function(event){
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(function(resposta){
+
+      return resposta || fetch(event.request);
+
+    })
+
+  );
+
+});
